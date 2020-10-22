466e1a81312b122aeb51112e331f3652a0ad2258
Maria Searle <maria.searle@deeperthanblue.co.uk>
2020-10-22 14:37:34 +0100

    changed things around so maxmin was below cloud icon

--- index.js
+++ index.js
@@ -27,11 +27,11 @@ function setQuery(e) {
             console.log(data);
             updateName(data.name)
             updateTemp(Math.round(data.main.temp))
-            updateMinTemp(Math.round(data.main.temp_min))
-            updateMaxTemp(Math.round(data.main.temp_max))
             updateFeelsLike(Math.round(data.main.feels_like))
             updateDescription(data.weather[0].description)
             updateIcon(data.weather[0].icon);
+            updateMinTemp(Math.round(data.main.temp_min))
+            updateMaxTemp(Math.round(data.main.temp_max))
         }
     }
         
@@ -47,14 +47,6 @@ function setQuery(e) {
             const tempElement = document.getElementById('temp');
             tempElement.textContent = temp + "C";
         }
-        const updateMinTemp = (mintemp) => {
-            const mintempElement = document.getElementById('mintemp');
-            mintempElement.textContent ="Minimum Temperature " + mintemp + "C";
-        }
-        const updateMaxTemp = (maxtemp) => {
-            const maxtempElement = document.getElementById('maxtemp');
-            maxtempElement.textContent ="Maximum Temperature " + maxtemp + "C";
-        }
         const updateFeelsLike = (feelslike) => {
             const feelslikeElement = document.getElementById('feelslike');
             feelslikeElement.textContent ="Feels like " + feelslike + "C";
@@ -66,4 +58,12 @@ function setQuery(e) {
         const updateIcon = (icon) => {
             const iconElement = document.getElementById('weatherIcon');
             iconElement.src = `icons/${icon}.png`; 
+        }
+        const updateMinTemp = (mintemp) => {
+            const mintempElement = document.getElementById('mintemp');
+            mintempElement.textContent ="Minimum Temperature " + mintemp + "C";
+        }
+        const updateMaxTemp = (maxtemp) => {
+            const maxtempElement = document.getElementById('maxtemp');
+            maxtempElement.textContent ="Maximum Temperature " + maxtemp + "C";
         }
